d8ba8f2fad64dd6da63b0d6de9408aa1ba92141c
mohdanas2411 <31250305+mohdanas2411@users.noreply.github.com>
2019-01-11 23:18:54 +0530

    Print using S.o.p

---
+++ PrintUsingSOP.java
@@ -0,0 +1,13 @@
+class S{
+	static class o{
+	  static public void p(String input){
+		System.out.println(input);
+		}
+	}
+}
+class PrintUsingSOP{
+	public static void main(String[] args) {
+		S.o.p("anas"+"ds");
+		S.o.p("ANSARI");
+	}
+}
